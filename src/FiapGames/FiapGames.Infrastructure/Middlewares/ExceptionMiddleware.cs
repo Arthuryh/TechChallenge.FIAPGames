ee49ff3dae9b307ecee2e88693f9c8882fa40bf0
Dalmasio <dalma@MAIN-PC>
2026-04-25 14:55:45 -0300

    add middleware base

---
+++ src/FiapGames/FiapGames.Infrastructure/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace FiapGames.Infrastructure.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            var objErro = new ErroResponseDto();
+
+            switch (ex)
+            {
+                case UnauthorizedAccessException:
+                    objErro.TituloErro = "Acesso não autorizado";
+                    objErro.Status = HttpStatusCode.Unauthorized;
+                    break;
+
+                case ArgumentException:
+                    objErro.TituloErro = $"Requisição inválida: {ex.Message}";
+                    objErro.Status = HttpStatusCode.BadRequest;
+                    break;
+
+                default:
+                    objErro.TituloErro = "Erro interno do servidor";
+                    objErro.Status = HttpStatusCode.InternalServerError;
+                    break;
+
+            }
+            //logica de log{}
+            context.Response.StatusCode = (int)objErro.Status;
+            await context.Response.WriteAsync(JsonSerializer.Serialize(objErro));
+        }
+
+        public class ErroResponseDto()
+        {
+            public string TituloErro { get; set; }
+            public HttpStatusCode Status { get; set; }
+        }
+    }
+}
--- src/FiapGames/FiapGames.WebApi/Program.cs
+++ src/FiapGames/FiapGames.WebApi/Program.cs
@@ -5,51 +5,102 @@ using FiapGames.Application.Interfaces.Login;
 using FiapGames.Application.Servicos;
 using FiapGames.Infrastructure.Contextos;
 using FiapGames.Infrastructure.Interfaces;
+using FiapGames.Infrastructure.Middlewares;
 using FiapGames.Infrastructure.Repositorios;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using static FiapGames.Infrastructure.Middlewares.ExceptionMiddleware;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("FIAPGamesConnection");
-
-builder.Services.AddDbContext<FIAPGamesContext>(opts =>
-    opts
-    .UseLazyLoadingProxies()
-    .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-builder.Services.AddScoped<ILoginRepositorio, LoginRepositorio>();
-builder.Services.AddScoped<ILoginServico, LoginServico>();
-
-builder.Services.AddScoped<IJogoRepositorio, JogoRepositorio>();
-builder.Services.AddScoped<IJogoServico, JogoServico>();
-
-builder.Services.AddScoped<ICompraRepositorio, CompraRepositorio>();
-builder.Services.AddScoped<ICompraServico, CompraServico>();
-
-builder.Services.AddScoped<IPromocaoRepositorio, PromocaoRepositorio>();
-builder.Services.AddScoped<IPromocaoServico, PromocaoServico>();
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = builder.Configuration.GetConnectionString("FIAPGamesConnection");
+
+        builder.Services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var erro = new
+                {
+                    erro = "Requisição inválida",
+                    status = 400,
+                    detalhes = context.ModelState
+                        .Where(x => x.Value.Errors.Count > 0)
+                        .ToDictionary(
+                            x => x.Key,
+                            x => x.Value.Errors.Select(e => e.ErrorMessage)
+                        )
+                };
+
+                return new BadRequestObjectResult(erro);
+            };
+        });
+
+        builder.Services.AddDbContext<FIAPGamesContext>(opts =>
+            opts
+            .UseLazyLoadingProxies()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        builder.Services.AddScoped<ILoginRepositorio, LoginRepositorio>();
+        builder.Services.AddScoped<ILoginServico, LoginServico>();
+
+        builder.Services.AddScoped<IJogoRepositorio, JogoRepositorio>();
+        builder.Services.AddScoped<IJogoServico, JogoServico>();
+
+        builder.Services.AddScoped<ICompraRepositorio, CompraRepositorio>();
+        builder.Services.AddScoped<ICompraServico, CompraServico>();
+
+        builder.Services.AddScoped<IPromocaoRepositorio, PromocaoRepositorio>();
+        builder.Services.AddScoped<IPromocaoServico, PromocaoServico>();
+
+        // Add services to the container.
+
+        builder.Services.AddControllers()
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var erros = context.ModelState
+                        .Where(x => x.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors
+                        .Select(e => $"{x.Key}: {e.ErrorMessage}"))
+                        .ToList();
+
+                    return new BadRequestObjectResult(new
+                    {
+                        erro = "Requisição inválida",
+                        status = (int)HttpStatusCode.BadRequest,
+                        detalhes = erros
+                    });
+                };
+            });
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
