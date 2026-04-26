ed69d11bd82ebcf98662c03d8aac558139650ed7
Dalmasio <dalma@MAIN-PC>
2026-04-26 17:39:45 -0300

    add log

---
+++ src/FiapGames/FiapGames.Infrastructure/Repositorios/LogsRepo/LogRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using FiapGames.Domain.Entidades;
+using FiapGames.Infrastructure.Contextos;
+using FiapGames.Infrastructure.Interfaces.Log;
+
+namespace FiapGames.Infrastructure.Repositorios.Logs
+{
+    public class LogRepositorio : ILogRepositorio
+    {
+        private readonly FIAPGamesContext _context;
+
+        public LogRepositorio(FIAPGamesContext context)
+        {
+            _context = context;
+        }
+        public async Task SalvarLogErro(LogError log)
+        {
+            await _context.Logs.AddAsync(log);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
